8751015fd941ab4d83f4c8b544456734ec41bd46
Xiong Chen <yiheqilin@users.noreply.github.com>
2018-04-23 16:10:23 +0800

    Update build-suse-plasma.sh

--- build-suse-plasma.sh
+++ build-suse-plasma.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export DIB_RELEASE="tumbleweed"
-export ELEMENTS_PATH=/home/yiheqilin/workshop/suse-tumbleweed/
+export ELEMENTS_PATH=~/workshop/suse-tumbleweed/
 export DIB_OPENSUSE_MIRROR="http://mirrors.tuna.tsinghua.edu.cn/opensuse"
 
 disk-image-create -a amd64 -t qcow2 --min-tmpfs 4 vm opensuse-minimal plasma -o $1
